c24153d2dafea211002e85fe218ad4272187cbf2
qinyuhang <qinyuhang@bytedance.com>
2022-10-22 14:30:31 +0800

    update

--- exercises/standard_library_types/iterators5.rs
+++ exercises/standard_library_types/iterators5.rs
@@ -10,8 +10,6 @@
 //
 // Make the code compile and the tests pass.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
@@ -34,6 +32,10 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
+    map.iter()
+        .filter(|(_, &p)| p == Progress::Complete)
+        .collect::<Vec<(&String, &Progress)>>()
+        .len()
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -52,6 +54,10 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
+    collection
+        .iter()
+        .map(|item| count_for(item, value))
+        .fold(0, |a, b| a + b)
 }
 
 #[cfg(test)]
